5769e758f4d623110749ff5a747ac838e037ad4c
hamandakuntz <hamanda.monteiro@hotmail.com>
2021-06-10 10:32:59 -0300

    implementaçao de feature postar um comentario

--- src/app.js
+++ src/app.js
@@ -13,12 +13,12 @@ let posts = [{
     contentPreview: 'Esta é a estrutura de um post esperado pelo front-end',
     content: 'Este é o conteúdo do post, o que realmente vai aparecer na página do post...',
     commentCount: 2,
-    comments: {
+    comments: [{
         id: 1,
         postId: 1,
         author: 'João',
         content: 'Muito bom esse post! Tá de parabéns'
-    },
+    }],
 },{
     id: 2,
     title: 'Hello World TESTANDO MAIS UMHARD CODED NO SERVER',
@@ -26,12 +26,12 @@ let posts = [{
     contentPreview: 'Esta é a estrutura de um post esperado pelo front-end',
     content: 'Este é o conteúdo do post, o que realmente vai aparecer na página do post...',
     commentCount: 2,
-    comments: {
+    comments: [{
         id: 1,
         postId: 2,
         author: 'Maria',
         content: 'Muito bom esse post! Tá de parabéns'
-    },
+    }],
 }];
 
 app.get("/posts", (req, res) => {
@@ -40,7 +40,7 @@ app.get("/posts", (req, res) => {
 
 app.get("/posts/:id", (req, res) => {
     const id = parseInt(req.params.id);    
-    const post = posts.filter((post) => post.id === id);     
+    const post = posts.find((post) => post.id === id);     
     res.send(post);      
 });
 
@@ -55,6 +55,15 @@ app.get("/posts/:id/comments", (req, res) => {
     const postClicked = posts.find((post) => post.id === id);    
     console.log(postClicked.comments);      
     res.send(postClicked.comments);
+});
+
+app.post("/posts/:id/comments", (req, res) => {
+    const id = parseInt(req.params.id);   
+    const postClicked = posts.find((post) => post.id === id);  
+    const commentsOf = postClicked.comments;    
+    const newComment = req.body;
+    commentsOf.push(newComment);
+    res.send(commentsOf)     
 });
 
 app.listen(4000);
